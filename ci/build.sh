46a8c7771d276e3cb48c0eab309ed7e2a77b1768
Vlad Korolev <vlad@v-lad.org>
2024-01-03 17:12:28 -0500

    Add build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,1 +1,10 @@
 echo Building RPM
+
+# Warning, because of this we can't build to rpms on the same build
+# server at the same time.  
+rm -rf ~/.rpmmacros
+echo '%_topdir ' $(pwd)  > ~/.rpmmacros
+rpmbuild -ba SPECS/redhat-lsb.spec
+
+tar -czf redhat-lsb-rpms.tgz
+
